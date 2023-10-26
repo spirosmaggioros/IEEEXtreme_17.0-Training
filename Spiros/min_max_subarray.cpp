7c6420c91bec57abafb314f7520ecc6fc16911ce
Spiros Maggioros <51701672+spirosmaggioros@users.noreply.github.com>
2023-10-26 21:24:55 +0300

    Update min_max_subarray.cpp

--- Spiros/min_max_subarray.cpp
+++ Spiros/min_max_subarray.cpp
@@ -92,21 +92,6 @@ using namespace std;
 typedef long long int int64;
 typedef unsigned long int uint32;
 
-//please dont look at this(from here) :)
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cerr << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
-#ifdef SPIROS_DEBUG
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
-
-//(till here)
-
 int minMaxSubarray(const vector<int>& v) {
     auto minim = *min_element(v.begin() , v.end());
     auto maxim = *max_element(v.begin() , v.end());
